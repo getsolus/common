2bad2409ea9b188703385fa6846a74154ebeec3d
Joey Riches <josephriches@gmail.com>
2023-08-18 17:07:09 +0100

    Go/update_packages: Update for phab -> github migration
    Usage: GITHUB_AUTH_TOKEN=`gh auth token` go run update_packages.go > ../packages

--- Go/update_packages.go
+++ Go/update_packages.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Solus Project <copyright@getsol.us>
+ * Copyright 2023 Solus Project <copyright@getsol.us>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,133 +14,90 @@
  * limitations under the License.
  */
 
+// USAGE: GITHUB_AUTH_TOKEN=`gh auth token` go run update_packages.go > ../packages
+
 package main
 
 import (
-	"encoding/json"
+	"context"
+	"errors"
 	"fmt"
-	"net/http"
-	"net/url"
-	"os"
-	"os/user"
+	"log"
 	"sort"
+	"os"
+
+	"github.com/google/go-github/v54/github"
 )
 
-const defaultHost = "https://dev.getsol.us/api/"
+func getRepos(ctx context.Context, client *github.Client, organizationName string) ([]*github.Repository, error) {
+	opt := &github.RepositoryListByOrgOptions{
+		Type: "public",
+		ListOptions: github.ListOptions{PerPage: 100},
+	}
 
-var excludes = []string{
-	"budgie-desktop-src",
-	"budgie-desktop-view-src",
-	"budgie-desktop-branding-src",
-	"gnome-desktop-branding-src",
-	"infrastructure-tooling",
-	"linux-next",
-	"plasma-desktop-branding-src",
-	"solus-appstream-data",
-	"solus-image-budgie",
-	"solus-image-gnome",
-	"solus-image-i3",
-	"solus-image-mate",
-	"solus-image-plasma",
-	"solus-image-xfce",
-	"solus-secureboot-keys",
-	"solus-site",
-	"solus-site-backend",
-	"solus-site-styling",
-	"solus-webplatform-js",
-	"ypkg-next-gen",
-	"ypkg-source",
-}
+	var rateErr *github.RateLimitError
+	var abuseErr *github.AbuseRateLimitError
 
-type ARCRC struct {
-	Hosts map[string]map[string]string `json:"hosts"`
-}
+	// get all pages of results
+	var allRepos []*github.Repository
+	for {
+		repos, resp, err := client.Repositories.ListByOrg(ctx, organizationName, opt)
 
-type Repo struct {
-	Fields struct {
-		ShortName string `json:"shortName"`
-	} `json:"fields"`
-}
+		if errors.As(err, &rateErr) {
+			log.Fatalln("hit rate limit")
+		}
+		if errors.As(err, &abuseErr) {
+			log.Fatalln("hit secondary rate limit")
+		}
+		if err != nil {
+			return nil, err
+		}
 
-type RepoResult struct {
-	Data   []Repo `json:"data"`
-	Cursor struct {
-		After string `json:"after"`
-	} `json:"cursor"`
-}
+		allRepos = append(allRepos, repos...)
+		log.Printf("Getting repos, page %d\n", opt.Page)
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.Page = resp.NextPage
+	}
 
-type RepoResults struct {
-	Result RepoResult `json:"result"`
+	// Filter archived repos
+	var active []*github.Repository
+	for _, repo := range allRepos {
+		if *repo.Archived == false {
+			active = append(active, repo)
+		}
+	}
+
+	//fmt.Printf("total repos found: %d\n", len(allRepos))
+	return active, nil
 }
 
 func main() {
-	u, err := user.Current()
+
+	ctx := context.Background()
+
+	token := os.Getenv("GITHUB_AUTH_TOKEN")
+	if token == "" {
+		log.Fatal("Unauthorized: No token present in GITHUB_AUTH_TOKEN")
+	}
+	client := github.NewTokenClient(ctx, token)
+
+	repos, err := getRepos(ctx, client, "solus-packages")
 	if err != nil {
-		panic(err.Error())
+		log.Fatalf("Failed to get repos, reason: %s\n", err)
 	}
-	config, err := os.Open(u.HomeDir + "/.arcrc")
-	if os.IsNotExist(err) {
-		config, err = os.Open(u.HomeDir + "/.config" + "/arcrc")
-	}
-	if err != nil {
-		panic(err.Error())
-	}
-	defer config.Close()
-	dec := json.NewDecoder(config)
-	var arcrc ARCRC
-	err = dec.Decode(&arcrc)
-	if err != nil {
-		panic(err.Error())
-	}
-	hostConfig, ok := arcrc.Hosts[defaultHost]
-	if !ok || len(hostConfig) == 0 {
-		panic("No host config found.")
-	}
-	token, ok := hostConfig["token"]
-	if !ok || len(token) == 0 {
-		panic("No host token found.")
-	}
-	repos := make([]string, 0)
-	var next string
-	var resp *http.Response
-	for {
-		if len(next) > 0 {
-			resp, err = http.PostForm(defaultHost+"diffusion.repository.search",
-				url.Values{"api.token": {token}, "after": {next}, "queryKey": {"active"}})
-		} else {
-			resp, err = http.PostForm(defaultHost+"diffusion.repository.search",
-				url.Values{"api.token": {token}, "queryKey": {"active"}})
-		}
-		if err != nil {
-			panic(err.Error())
-		}
-		dec2 := json.NewDecoder(resp.Body)
-		var results RepoResults
-		err = dec2.Decode(&results)
-		resp.Body.Close()
-		if err != nil {
-			panic(err.Error())
-		}
-		for _, repo := range results.Result.Data {
-			excluded := false
-			for _, e := range excludes {
-				if e == repo.Fields.ShortName {
-					excluded = true
-					break
-				}
-			}
-			if excluded {
-				continue
-			}
-			repos = append(repos, repo.Fields.ShortName)
-		}
-		next = results.Result.Cursor.After
-		if len(next) == 0 {
-			break
-		}
-	}
-	sort.Strings(repos)
+
+	var repoNames []string
 	for _, repo := range repos {
-		fmt.Println(repo)
+		if *repo.Name != ".github" {
+			repoNames = append(repoNames, *repo.Name)
+		}
+	}
+
+	sort.Strings(repoNames)
+
+	for _, name := range repoNames {
+		fmt.Println(name)
 	}
 }
