f0dfeeafe5776659c4d8d9fcdb3acc65598f08b2
Troy Harvey <harveydevel@gmail.com>
2023-09-13 14:28:44 +1000

    Add more features to go-task
    - pkgconfig search
    - clone
    - clone-all (Multi-threaded)
    - update-tooling

---
+++ Scripts/clone-all.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess as sp
+import concurrent.futures
+import argparse
+
+def clone_all(base_repo, pkg, failed_pkgs):
+    try:
+        if os.path.exists(pkg):
+            sp.run(["git", "-C", f"{pkg}", "pull"], check=True)
+        else:
+            sp.run(["git", "clone", f"https://{base_repo}{pkg}.git"], check=True)
+            sp.run(["git", "-C", f"{pkg}", "remote", "set-url", "origin", f"https://{base_repo}{pkg}.git"])
+            sp.run(["git", "-C", f"{pkg}", "remote", "set-url", "--push", "origin", f"git@{base_repo}{pkg}.git"])
+    except Exception:
+        failed_pkgs.append(pkg)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-j",
+        type=int,
+        default=1,
+        choices=range(1, 31),
+        help="Set the number of concurrent jobs e.g -j20"
+    )
+    args = parser.parse_args()
+
+    pkgs_fpath = "common/packages"
+    pkgs = []
+    base_repo = "github.com/solus-packages/"
+
+    try:
+        with open(pkgs_fpath, "r") as file:
+            for line in file:
+                pkgs.append(line.strip())
+    except FileNotFoundError:
+        print(f"File not found: {pkgs_fpath}")
+
+    try:
+        while True:
+            failed_pkgs = []
+
+            with concurrent.futures.ThreadPoolExecutor(max_workers=args.j) as executor:
+                futures = {executor.submit(clone_all, base_repo, pkg, failed_pkgs) for pkg in pkgs}
+
+            if not failed_pkgs:
+                break
+
+            print(f"\n{failed_pkgs} packages failed")
+            retry = input("Would you like to retry (y/n?)\n").lower()
+            if retry not in ["yes", "y"]:
+                break
+    except KeyboardInterrupt:
+        executor.shutdown(cancel_futures=True)
+        print("Script terminated by ctrl+c.")
+
+main()
